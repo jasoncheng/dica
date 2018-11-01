29da0de56eeb547ef45622695e053613c941f0c6
jasoncheng <jason@brandma.co>
2018-11-01 17:14:11 +0800

    1. re-layout status item 2. add status created time (relative datetime)

--- app/src/main/java/link/mawa/android/adapter/StatusesAdapter.kt
+++ app/src/main/java/link/mawa/android/adapter/StatusesAdapter.kt
@@ -2,14 +2,16 @@ package link.mawa.android.adapter
 
 import android.content.Context
 import android.support.v7.widget.RecyclerView
-import android.util.Log
+import android.text.format.DateUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import kotlinx.android.synthetic.main.status_list_item.view.*
 import link.mawa.android.R
 import link.mawa.android.bean.Status
+import java.util.*
 
 class StatusesAdapter(val data:ArrayList<Status>, val context: Context): RecyclerView.Adapter<StatusViewHolder>() {
 
@@ -17,8 +19,12 @@ class StatusesAdapter(val data:ArrayList<Status>, val context: Context): Recycle
         val st = data.get(position)
         holder.userName.text = st.user.screen_name
         holder.content.text = st.text
-        Log.i("GGGG", "=================> ${st.user.screen_name} ${st.text}")
-        Glide.with(context.applicationContext).load(st.user.profile_image_url_large).into(holder.avatar)
+        var date = Date(st.created_at)
+        holder.datetime.text = DateUtils.getRelativeTimeSpanString(date.time)
+        Glide.with(context.applicationContext)
+            .load(st.user.profile_image_url_large)
+            .apply(RequestOptions().circleCrop())
+            .into(holder.avatar)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StatusViewHolder {
@@ -41,4 +47,5 @@ class StatusViewHolder(view: View): RecyclerView.ViewHolder(view) {
     val userName = view.tv_status_user_name
     val avatar = view.avatar
     var content = view.tv_content
+    var datetime = view.tv_datetime
 }
