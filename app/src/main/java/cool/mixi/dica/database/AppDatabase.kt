8d68cdc48e90c5701e0d292da70276ee4251f887
jasoncheng <17hado.com@gmail.com>
2018-12-18 19:09:06 +0800

    temp update

--- app/src/main/java/cool/mixi/dica/database/AppDatabase.kt
+++ app/src/main/java/cool/mixi/dica/database/AppDatabase.kt
@@ -4,6 +4,8 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import cool.mixi.dica.App
 import cool.mixi.dica.bean.Consts
 import cool.mixi.dica.bean.Meta
@@ -11,6 +13,7 @@ import cool.mixi.dica.bean.User
 import cool.mixi.dica.database.dao.MetaDao
 import cool.mixi.dica.database.dao.UserDao
 import cool.mixi.dica.util.TimestampConverter
+import cool.mixi.dica.util.dLog
 import java.util.*
 
 @Database(entities = [Meta::class, User::class], version = 2, exportSchema = false)
@@ -31,6 +34,7 @@ abstract class AppDatabase: RoomDatabase() {
                     AppDatabase::class.java,
                     Consts.DB_NAME
                 )
+                    .addMigrations(MigrationDb(2, 3))
                     .fallbackToDestructiveMigration()
                     .build()
                 db!!
@@ -43,4 +47,11 @@ abstract class AppDatabase: RoomDatabase() {
             return calendar.time
         }
     }
+
+    class MigrationDb(startVersion:Int, endVersion:Int): Migration(startVersion, endVersion) {
+        override fun migrate(database: SupportSQLiteDatabase) {
+            dLog("migration DB")
+        }
+    }
+
 }
